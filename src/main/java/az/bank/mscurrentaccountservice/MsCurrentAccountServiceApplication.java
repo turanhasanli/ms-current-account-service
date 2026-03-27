66289516968954118141a83dde374eea35098fb1
Turan <turan.hesenli98@gmail.com>
2026-03-28 00:01:48 +0400

    fix:add @EnableScheduling annotation

--- src/main/java/az/bank/mscurrentaccountservice/MsCurrentAccountServiceApplication.java
+++ src/main/java/az/bank/mscurrentaccountservice/MsCurrentAccountServiceApplication.java
@@ -3,9 +3,11 @@ package az.bank.mscurrentaccountservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableScheduling
 public class MsCurrentAccountServiceApplication {
 
     public static void main(String[] args) {
